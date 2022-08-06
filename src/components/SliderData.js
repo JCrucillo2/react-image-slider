0cda80855186579e560c610c0fc710dd0590e782
Junnel Crucillo <71023403+JCrucillo2@users.noreply.github.com>
2022-08-06 04:06:10 -0600

    Finished image slider

---
+++ src/components/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+    {
+        image: "https://images.unsplash.com/photo-1659636688061-6ce8e846f6dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1659539457644-17cf3ee3ad4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1659520070509-de22d374a122?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1659124172725-104c5d01ba71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1659124172741-96b753e526ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80",
+    },
+];
